25cf404449666feef4f6915f363338ad4163a761
miguelvalden <miguelvalden77@gmail.com>
2022-10-25 02:38:17 +0200

    icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="shortcut icon" href="/morty.png">
     <title>Rick & Morty</title>
 </head>
 <body class="body">
